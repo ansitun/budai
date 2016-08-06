b80fd6dec27498566c62c5c24c50961b18113615
Mindfire <satheeskumara@mindfiresolutions.com>
2016-08-07 02:24:31 +0530

    Adding DB modifications

---
+++ app/DoctrineMigrations/Version20160807022328.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20160807022328 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE Address ADD last_update_date_time DATETIME DEFAULT NULL');
+        $this->addSql('ALTER TABLE User CHANGE user_type user_type ENUM(\'adm\', \'ven\', \'cust\')');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE Address DROP last_update_date_time');
+        $this->addSql('ALTER TABLE User CHANGE user_type user_type VARCHAR(255) DEFAULT NULL COLLATE utf8_unicode_ci');
+    }
+}
