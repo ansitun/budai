c8ec3f0f393108e557b51d91a1436f2ec8cebd91
Mindfire <satheeskumara@mindfiresolutions.com>
2016-08-07 03:17:04 +0530

    Adding product admin page

--- src/AppBundle/Admin/ProductAdmin.php
+++ src/AppBundle/Admin/ProductAdmin.php
@@ -1,12 +1,13 @@
 <?php
 
 /**
- * BlogCategory Admin file for Sonata Bundle.
+ * Product admin page
  *
  * @author
  *
  * @category Admin
  */
+
 namespace AppBundle\Admin;
 
 use AppBundle\Admin\BaseAdmin;
@@ -14,44 +15,55 @@ use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 
-class ProductAdmin extends BaseAdmin
-{
+class ProductAdmin extends BaseAdmin {
+
     // Fields too be shown on create/edit forms
-    protected function configureFormFields(FormMapper $formMapper)
-    {
+    protected function configureFormFields(FormMapper $formMapper) {
         $formMapper
-            ->add('name')
-            ->add('description')  
-            ->add('status', 'entity', array('class' => 'AppBundle\Entity\Status', 
+                ->add('name')
+                ->add('description')
+                ->add('sku')
+                ->add('price')
+                ->add('discount_price')
+                ->add('tax')
+                ->add('key_words')
+                ->add('image_url')
+                ->add('image_url2')
+                ->add('image_url3')
+                ->add('image_url4')
+                ->add('thumbnail_url')
+                ->add('status', 'entity', array('class' => 'AppBundle\Entity\Status',
                     'property' => 'string_value',
-                    'empty_data'  => null,
+                    'empty_data' => null,
                     'empty_value' => "Please select a Status",
-                    ))      
+                ))
         ;
     }
 
     // Fields to be shown on filter forms
-    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
-    {
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper) {
         $datagridMapper
-            ->add('name')
-            ->add('description')  
-            ->add('status.string_value')    
+                ->add('name')
+                ->add('description')
+                ->add('status.string_value')
         ;
     }
 
     // Fields to be shown on lists
-    protected function configureListFields(ListMapper $listMapper)
-    {
+    protected function configureListFields(ListMapper $listMapper) {
         $listMapper
-            ->add('name')
-            ->add('description')  
-            ->add('status.string_value')
-            ->add('_action', 'actions', array(
-                'actions' => array(
-                    'edit' => array(),
-                ),
-            ))
+                ->add('name')
+                ->add('description')
+                ->add('sku')
+                ->add('price')
+                ->add('discount_price')
+                ->add('status.string_value')
+                ->add('_action', 'actions', array(
+                    'actions' => array(
+                        'edit' => array(),
+                    ),
+                ))
         ;
     }
+
 }
